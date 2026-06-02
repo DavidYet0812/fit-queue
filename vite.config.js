c9fc0c31d1457e87eb33ddaa15c0ed88d47a6161
DavidYet0812 <s714139@gmail.com>
2026-06-02 20:11:38 +0800

    Add GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/fit-queue/",
   plugins: [react()],
   build: {
     rollupOptions: {
